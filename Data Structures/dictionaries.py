98ad2342ac6c2f511f6ce60452ace6ab249b4e56
Ivan <ivan.01.arabadzhiev@gmail.com>
2022-10-04 23:41:02 +0100

    Update dictionaries.py

--- Data Structures/dictionaries.py
+++ Data Structures/dictionaries.py
@@ -5,8 +5,11 @@ print(dict['age'])
 # The keys are usually strings, but it can be otherwise.
 
 dict['age'] = 85
+print(dict['age'])
 
+dict['age'] += 1
 print(dict['age'])
 print(dict.keys())
+
 # Turns the dictionary into a list of tuples.
 print(dict.items())
